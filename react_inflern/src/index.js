1f62bbed5d1bb92aaa48996bedbbf840abc1c1e0
eeeyooon <vywns9978@gmail.com>
2022-09-20 14:52:39 +0900

    [first-met] ch05. Components and Props

--- react_inflern/src/index.js
+++ react_inflern/src/index.js
@@ -3,17 +3,16 @@ import { createRoot } from "react-dom/client";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import Clock from './ch04/Clock';
+import CommentList from './ch05/CommentList';
 
 //리액트 18버전 코드 (강의와 코드 다름.)
 const root = createRoot(document.getElementById('root'));
 
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-}, 1000);
+root.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 reportWebVitals();
